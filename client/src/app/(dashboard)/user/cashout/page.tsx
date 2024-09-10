3739ac7d041b888f0cd84f5079bd0474155ff3cd
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-09-10 22:28:55 +0600

    cash out page created custom like send money

--- client/src/app/(dashboard)/user/cashout/page.tsx
+++ client/src/app/(dashboard)/user/cashout/page.tsx
@@ -1,11 +1,16 @@
+'use client'
 import ActionBar from "@/components/UI/ActionBar";
 import CashoutPage from "@/components/UI/Cashout";
+import { useProfileQuery } from "@/redux/api/userApi";
+import { getUserInfo } from "@/services/auth.service";
 
 const Cashout = () => {
+    const { userId } = getUserInfo() as any;
+  const { data: userData } = useProfileQuery(userId);
   return (
     <div>
       <ActionBar title="Cashout" />
-      <CashoutPage/>
+      <CashoutPage userData={userData}/>
     </div>
   );
 };
