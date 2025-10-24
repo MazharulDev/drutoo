be6f98e7d9c63040188626a17280f2ce6dac8a26
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2025-10-24 17:25:12 +0600

    reinit expo project

--- drutoo-app/app/(dashboard)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function DashboardLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="home" options={{ headerShown: true, title: 'Dashboard' }} />
-        </Stack>
-    );
-}
--- drutoo-app/app/constants/storageKey.ts
+++
@@ -1,1 +0,0 @@
-export const authkey = 'drutoo-auth-token';
